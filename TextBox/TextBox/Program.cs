c4d198b7bbe186b41a9972545284a185e45c17de
ghern <ghern@DESKTOP-OCUVVH0>
2021-11-30 11:36:40 -0600

    Completed

---
+++ TextBox/TextBox/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox one = new TextBox();
+            one.Text = "Hello World";
+            one.Padding = 5;
+            one.BackColor = ConsoleColor.Gray;
+            one.ForeColor = ConsoleColor.Blue;
+            one.DisplayText();
+        }
+    }
+}
---
+++ TextBox/TextBox/TextBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public string Text { get; set; }
+        public TextBox()
+        {
+            Padding = 0;
+            BackColor = ConsoleColor.Red;
+            ForeColor = ConsoleColor.Green;
+            Text = string.Empty;
+        }
+        public void DisplayText()
+        {
+            string padding = " ";
+            for (int i = 0; i < Padding; i++)
+            {
+                padding += padding;
+            }
+            string result = padding + Text + padding;
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            Console.WriteLine(result);
+            //Text.PadLeft(Padding, ' ');
+            //Console.WriteLine(Text);
+            //Text.PadRight(Padding, ' ');
+        }
+    }
+}
